924d608b8f2ef0ac74eef8b5878ae70c40326c22
prabirstack <160392884+prabirstack@users.noreply.github.com>
2024-04-08 18:18:38 +0530

    Stae 3: Site Header

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: 'PrabirBlog',
+  url: 'https://example.com',
+  description: 'Nextjs 14 blog using velite, tailwind, and shadcn ',
+  author: 'Prabir Singh',
+  links: {
+    twitter: 'https://twitter.com/prabirshub',
+    github: 'https://github.com/prabirshub/prabirshub.github.io',
+    personalSite: 'https://www.jamesshopland.com/',
+  },
+}
+
+export type SiteConfig = typeof siteConfig
