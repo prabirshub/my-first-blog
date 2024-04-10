9be66a73d35d3fbe38148fd11187cdc6a45bea13
prabirstack <160392884+prabirstack@users.noreply.github.com>
2024-04-10 12:16:36 +0530

    Stage 18: Add tags card

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-import { Post } from '#site/content'
+import { Post, posts } from '#site/content'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
@@ -21,4 +21,19 @@ export function sortPosts(posts: Array<Post>) {
     if (a.date < b.date) return 1
     return 0
   })
+}
+
+export function getAllTags(post: Array<Post>) {
+  const tags: Record<string, number> = {}
+  posts.forEach((post) => {
+    post.tags?.forEach((tag) => {
+      tags[tag] = (tags[tag] ?? 0) + 1
+    })
+  })
+
+  return tags
+}
+
+export function sorTagsByCount(tags: Record<string, number>) {
+  return Object.keys(tags).sort((a, b) => tags[b] - tags[a])
 }
